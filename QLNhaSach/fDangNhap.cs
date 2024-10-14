84e0f1dd099f4b943a38944c699053280a6ea04d
Duy2902 <tduy29.2k4@gmail.com>
2024-10-14 18:27:05 +0700

    chiu r

--- QLNhaSach/fDangNhap.cs
+++ QLNhaSach/fDangNhap.cs
@@ -17,7 +17,7 @@ namespace QLNhaSach
         public fDangNhap()
         {
             InitializeComponent();
-            txtMatKhau.UseSystemPasswordChar = true;
+            txtMatKhau.UseSystemPasswordChar = false;
         }
 
         private void lblMatKhau_Click(object sender, EventArgs e)
